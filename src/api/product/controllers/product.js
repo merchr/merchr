46039ef125924d48694e7443c55fd6df4d6724e5
blendh <b.hamiti.9@gmail.com>
2021-12-26 17:08:29 +0100

    .

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -6,4 +6,29 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController('api::product.product', ({ strapi }) => ({
+    async find(ctx) {
+        // Calling the default core action
+        const { meta } = await super.find(ctx);
+
+        // some custom logic here
+        const data = await strapi.entityService.findMany('api::product.product', {
+            populate: {
+                category: {
+                    fields: ['name', 'description']
+                },
+                color: {
+                    fields: ['name', 'hex']
+                },
+                sex: {
+                    fields: ['type']
+                },
+                size: {
+                    fields: ['name']
+                },
+            },
+        });
+
+        return { data, meta };
+    },
+}));
