417d7ff4b9e7a68c5a9a439e6fe2f36280716619
Blend Hamiti <b.hamiti.9@gmail.com>
2022-01-15 23:40:35 +0100

    .

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -1,34 +1,34 @@
-'use strict';
+"use strict";
 
 /**
  *  product controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::product.product', ({ strapi }) => ({
-    async find(ctx) {
-        // Calling the default core action
-        const { meta } = await super.find(ctx);
+module.exports = createCoreController("api::product.product", ({ strapi }) => ({
+  async find(ctx) {
+    // Calling the default core action
+    const { meta } = await super.find(ctx);
 
-        // some custom logic here
-        const data = await strapi.entityService.findMany('api::product.product', {
-            populate: {
-                category: {
-                    fields: ['name', 'description']
-                },
-                color: {
-                    fields: ['name', 'hex']
-                },
-                sex: {
-                    fields: ['type']
-                },
-                size: {
-                    fields: ['name']
-                },
-            },
-        });
+    // some custom logic here
+    const data = await strapi.entityService.findMany("api::product.product", {
+      populate: {
+        category: {
+          fields: ["name", "description", "price"],
+        },
+        color: {
+          fields: ["name", "hex"],
+        },
+        sex: {
+          fields: ["type"],
+        },
+        size: {
+          fields: ["name"],
+        },
+      },
+    });
 
-        return { data, meta };
-    },
+    return { data, meta };
+  },
 }));
